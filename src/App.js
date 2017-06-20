d66648c2df079a47b86d03f2cc27fa814c9f2153
Keenan Goldis <keenangoldis@gmail.com>
2017-06-20 18:21:52 -0500

    checkpoint2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      api: "",
+    }
+  }
+
+  componentWillMount() {
+    //Make a request for a user with a given ID
+    axios.post('http://posttestserver.com/post.php', 'This is a test sentence')
+    .then ((response) => {
+      console.log(response);
+      this.setState({apiPic: response.data})
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Posting Apis</h2>
+          <img src={this.state.apiPic} className="App-logo" alt="logo" />
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.apiPic}
         </p>
       </div>
     );
